1f9ed96ea52a19a6aa0810c7a27b67442f1b85af
gouyanming <gouyanming@mi-ya.com.cn>
2020-09-17 15:13:34 +0800

    feat: 新增tag标签

---
+++ src/my-tag/demo/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="tags">
+    <my-tag typeTag="one-tag">一级标签</my-tag>
+    <my-tag typeTag="two-tag">二级标签</my-tag>
+    <my-tag typeTag="two-tag" color="green" text-color="black">一级变色标签</my-tag>
+  </view>
+</template>
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+    }
+  },
+  onLoad() {
+    console.log('my-tag')
+  },
+}
+</script>
+<style scoped lang="less">
+.tags {
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ src/my-tag/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="one-tag" :class="typeTag" :style="{background:color,color:textColor}">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'my-tag',
+  props: {
+    typeTag: {
+      type: String,
+      default: 'one-tag'
+    },
+    color: {
+      type: String,
+      // default: 'none'
+    },
+    textColor: {
+      type: String,
+      // default: 'none'
+    }
+  },
+  computed: {},
+  data() {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+<style scoped lang="less">
+.one-tag {
+  width: auto;
+  display: inline-block;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 16px;
+  background: rgba(255,119,0,1);
+  border-radius: 20px;
+  font-size: 20px;
+  font-family: PingFangSC-Regular,PingFang SC;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+}
+.two-tag {
+  background: rgba(255,119,0,0.19829999999999998);
+  color: rgba(255,119,0,1);
+}
+</style>
