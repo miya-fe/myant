01b2c731a7006e4754887f9a7a951cacc8cca76a
yangxun <649658707@qq.com>
2021-01-25 19:05:43 +0800

    fix: 修复索引tab的点击事件bug

--- lib/my-index-bar/index.vue
+++ lib/my-index-bar/index.vue
@@ -103,6 +103,7 @@ export default class IndexBar extends Vue {
     top: 0;
     right: 0;
     bottom: 0;
+    z-index: 2;
     display: flex;
     flex-direction: column;
     align-items: center;
