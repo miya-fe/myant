bb8ef4f802fd340081bb2da76d7588118664bd63
yangxun <649658707@qq.com>
2021-01-29 10:50:53 +0800

    feat: 优化my-button组件兼容性

--- packages/myant-cli/sites/mini-program/src/demos/my-button/index.vue
+++ packages/myant-cli/sites/mini-program/src/demos/my-button/index.vue
@@ -1,6 +1,26 @@
 <template>
   <view>
     <view class="list">
+      <view class="tip"> 作为容器（wrapper=true） </view>
+      <view class="item">
+        <view class="btn-group">
+          <my-button wrapper open-type="getPhoneNumber">
+            <view class="btn">
+              <my-icon>&#xe603;</my-icon>
+              <text>点击获取手机号</text>
+            </view>
+          </my-button>
+          <my-button wrapper open-type="getUserInfo">
+            <view class="btn">
+              <my-icon>&#xe603;</my-icon>
+              <text>点击获取用户信息</text>
+            </view>
+          </my-button>
+        </view>
+      </view>
+    </view>
+
+    <view class="list">
       <view class="tip"> 默认大小按钮（size=default） </view>
       <view class="item">
         <my-button type="primary">primary</my-button>
@@ -103,6 +123,20 @@ export default {
   .item {
     margin-bottom: 20rpx;
     text-align: center;
+    .btn-group {
+      display: flex;
+      flex-direction: row;
+    }
+    .btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 10rpx 20rpx;
+      color: #fff;
+      font-size: 28rpx;
+      background-color: #f0ad4e;
+      border-radius: 50rpx;
+    }
   }
 }
 </style>
